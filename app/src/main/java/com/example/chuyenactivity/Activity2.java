b00c2fe6c3e8de0b3bbf1335b496253f3623a24d
Thquan105 <thquan105@gmail.com>
2022-09-08 18:37:07 +0700

    Create Event Button Back Activity 1

--- app/src/main/java/com/example/chuyenactivity/Activity2.java
+++ app/src/main/java/com/example/chuyenactivity/Activity2.java
@@ -2,7 +2,10 @@ package com.example.chuyenactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Button btBack = (Button) findViewById(R.id.btnBack);
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(Activity2.this,MainActivity.class);
+                startActivity(iLogin);
+            }
+        });
     }
 }
