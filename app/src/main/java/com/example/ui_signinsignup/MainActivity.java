0a66086791d7990713e236ef2787ee38f72b2512
Thquan105 <thquan105@gmail.com>
2022-09-15 21:53:37 +0700

    Event back activity login

--- app/src/main/java/com/example/ui_signinsignup/MainActivity.java
+++ app/src/main/java/com/example/ui_signinsignup/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ui_signinsignup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView btBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //      Event Back
+        btBack = (ImageView) findViewById(R.id.btnBack);
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ui_signinsignup/Signup_Activity.java
+++ app/src/main/java/com/example/ui_signinsignup/Signup_Activity.java
@@ -3,12 +3,22 @@ package com.example.ui_signinsignup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Signup_Activity extends AppCompatActivity {
-
+    private TextView txtview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_layout);
+        //      Event Sign In
+        txtview = (TextView) findViewById(R.id.txtLogin);
+        txtview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
