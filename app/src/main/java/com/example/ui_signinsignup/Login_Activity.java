d23d6454ad223ca0f03c07e0989d5c1196e6ea12
Thquan105 <thquan105@gmail.com>
2022-09-15 11:29:17 +0700

    Create Login Activity and Sign Up Activity

--- app/src/main/java/com/example/ui_signinsignup/Login_Activity.java
+++ app/src/main/java/com/example/ui_signinsignup/Login_Activity.java
@@ -1,14 +1,13 @@
-package com.example.ui_profile;
+package com.example.ui_signinsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
     }
 }
---
+++ app/src/main/java/com/example/ui_signinsignup/Signup_Activity.java
@@ -0,0 +1,14 @@
+package com.example.ui_signinsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
