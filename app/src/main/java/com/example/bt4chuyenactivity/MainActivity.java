705e97875e2250e396fedde581344e4569ea1c06
Thquan105 <thquan105@gmail.com>
2022-09-16 16:18:57 +0700

    create event first Activity

---
+++ app/src/main/java/com/example/bt4chuyenactivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bt4chuyenactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btnClickme);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
