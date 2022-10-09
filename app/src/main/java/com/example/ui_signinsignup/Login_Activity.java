1fc68668387c6dc01cd9e92fa3ad82ec9396ded1
Thquan105 <thquan105@gmail.com>
2022-10-10 01:10:18 +0700

    Event Back to Exit app

--- app/src/main/java/com/example/ui_signinsignup/Login_Activity.java
+++ app/src/main/java/com/example/ui_signinsignup/Login_Activity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 public class Login_Activity extends AppCompatActivity {
     private Button btSignIn;
     private TextView txtview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +29,13 @@ public class Login_Activity extends AppCompatActivity {
                 if (username.getText().toString().equals("quan") && password.getText().toString().equals("abc"))
                 {
                     Toast.makeText(Login_Activity.this,"Login Successful",Toast.LENGTH_SHORT).show();
-                    openProfile();
+                    openMenu();
+                    finish();
                 }
                 else
                 {
                     Toast.makeText(Login_Activity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                    username.requestFocus();
                 }
             }
         });
@@ -44,8 +48,27 @@ public class Login_Activity extends AppCompatActivity {
             }
         });
     }
-    public void openProfile(){
-        Intent intent = new Intent(this, MainActivity.class);
+    Boolean doubleTap = false;
+    @Override
+    public void onBackPressed() {
+        if (doubleTap) {
+            super.onBackPressed();
+            return;
+        } else {
+            Toast.makeText(Login_Activity.this, "Press Back again to exit the application!", Toast.LENGTH_SHORT).show();
+            doubleTap = true;
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleTap = false;
+                }
+            }, 1800);
+        }
+    }
+
+    public void openMenu(){
+        Intent intent = new Intent(this, Menu_Activity.class);
         startActivity(intent);
     }
     public void openSignUp(){
