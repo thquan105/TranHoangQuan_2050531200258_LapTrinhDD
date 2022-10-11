baa898c5f42b9be8cde5c7a116b7cdde2db3e284
Thquan105 <thquan105@gmail.com>
2022-10-11 13:38:18 +0700

    add class Page

---
+++ app/src/main/java/com/example/ui_signinsignup/Page.java
@@ -0,0 +1,37 @@
+package com.example.ui_signinsignup;
+
+public class Page {
+    private int srcImg;
+    private String txt_Name;
+    private String txt_Description;
+
+    public Page(int srcImg, String txt_Name, String txt_Description) {
+        this.srcImg = srcImg;
+        this.txt_Name = txt_Name;
+        this.txt_Description = txt_Description;
+    }
+
+    public int getSrcImg() {
+        return srcImg;
+    }
+
+    public void setSrcImg(int srcImg) {
+        this.srcImg = srcImg;
+    }
+
+    public String getTxt_Name() {
+        return txt_Name;
+    }
+
+    public void setTxt_Name(String txt_Name) {
+        this.txt_Name = txt_Name;
+    }
+
+    public String getTxt_Description() {
+        return txt_Description;
+    }
+
+    public void setTxt_Description(String txt_Description) {
+        this.txt_Description = txt_Description;
+    }
+}
