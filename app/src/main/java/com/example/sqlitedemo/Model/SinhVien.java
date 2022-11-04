1e03366dd91c8bae01349b7cfd800bb875571103
Thquan105 <thquan105@gmail.com>
2022-11-04 12:33:43 +0700

    Create class Sinh Vien

---
+++ app/src/main/java/com/example/sqlitedemo/Model/SinhVien.java
@@ -0,0 +1,47 @@
+package com.example.sqlitedemo.Model;
+
+public class SinhVien {
+    private String maSV_258;
+    private String tenSV_258;
+    private String khoa_258;
+    private String maLop_258;
+
+    public SinhVien(String maSV_258, String tenSV_258, String khoa_258, String maLop_258) {
+        this.maSV_258 = maSV_258;
+        this.tenSV_258 = tenSV_258;
+        this.khoa_258 = khoa_258;
+        this.maLop_258 = maLop_258;
+    }
+
+    public String getMaSV_258() {
+        return maSV_258;
+    }
+
+    public void setMaSV_258(String maSV_258) {
+        this.maSV_258 = maSV_258;
+    }
+
+    public String getTenSV_258() {
+        return tenSV_258;
+    }
+
+    public void setTenSV_258(String tenSV_258) {
+        this.tenSV_258 = tenSV_258;
+    }
+
+    public String getKhoa_258() {
+        return khoa_258;
+    }
+
+    public void setKhoa_258(String khoa_258) {
+        this.khoa_258 = khoa_258;
+    }
+
+    public String getMaLop_258() {
+        return maLop_258;
+    }
+
+    public void setMaLop_258(String maLop_258) {
+        this.maLop_258 = maLop_258;
+    }
+}
