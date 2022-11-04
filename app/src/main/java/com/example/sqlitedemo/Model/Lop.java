719d222b2e135b41fa07efc66961e3ff8d68a0cf
Thquan105 <thquan105@gmail.com>
2022-11-04 12:33:59 +0700

    Create class Lop

---
+++ app/src/main/java/com/example/sqlitedemo/Model/Lop.java
@@ -0,0 +1,37 @@
+package com.example.sqlitedemo.Model;
+
+public class Lop {
+    private String malop_258;
+    private String tenlop_258;
+    private int siso_258;
+
+    public Lop(String malop_258, String tenlop_258, int siso_258) {
+        this.malop_258 = malop_258;
+        this.tenlop_258 = tenlop_258;
+        this.siso_258 = siso_258;
+    }
+
+    public String getMalop_258() {
+        return malop_258;
+    }
+
+    public void setMalop_258(String malop_258) {
+        this.malop_258 = malop_258;
+    }
+
+    public String getTenlop_258() {
+        return tenlop_258;
+    }
+
+    public void setTenlop_258(String tenlop_258) {
+        this.tenlop_258 = tenlop_258;
+    }
+
+    public int getSiso_258() {
+        return siso_258;
+    }
+
+    public void setSiso_258(int siso_258) {
+        this.siso_258 = siso_258;
+    }
+}
